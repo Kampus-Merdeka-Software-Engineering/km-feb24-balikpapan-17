623ccad45c79a9b26df339e7c170c1252a9b0fb6
adit <akunpengganti.aditya@gmail.com>
2024-05-29 04:17:04 +0700

    hide api key test!

--- js/auth.js
+++ js/auth.js
@@ -1,15 +1,3 @@
-// auth.js
-const firebaseConfig = {
-  apiKey: "AIzaSyDCW0iJbOjSfZ8QYuPhDTJQi6Hq6SuOLXI",
-  authDomain: "km-balikpapan-team17.firebaseapp.com",
-  databaseURL:
-    "https://km-balikpapan-team17-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "km-balikpapan-team17",
-  storageBucket: "km-balikpapan-team17.appspot.com",
-  messagingSenderId: "621633907233",
-  appId: "1:621633907233:web:0f02c8b05222da50b8d160",
-};
-
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
